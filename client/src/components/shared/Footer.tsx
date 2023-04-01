eef6fcf47e5c1969aa41ee269f8d282a6ac3a00e
Ala Baganne <alabaganne9@gmail.com>
2023-04-01 21:45:01 +0100

    Code navbar, left and right sidebars

--- client/src/components/shared/Footer.tsx
+++ client/src/components/shared/Footer.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="px-10 py-2 text-sm border-t text-gray-500 text-center">
+      All rights reserved 2022-2023
+    </div>
+  );
 };
 
 export default Footer;
